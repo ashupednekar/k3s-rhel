30444ce9bbe2796b7e36cf8883317466cb2a697d
ashupednekar <ashutoshpednekar15@gmail.com>
2025-01-31 18:18:06 +0530

    chore: updated master.sh, permission ranchar k3s.yaml order

--- master.sh
+++ master.sh
@@ -5,12 +5,12 @@ sudo chown -R $USER:$USER /home/vagrant/apiserver-details
 export MASTER_IP=$(ip a | grep global | grep -v '10.0.2.15' | awk '{print $2}' | cut -f1 -d '/')
 echo $MASTER_IP > /home/vagrant/apiserver-details/master-ip
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy --disable traefik --disable metrics-server' sh -
 
 #apiserver details
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/apiserver-details/k3s.yaml
-sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 sudo chown $USER:$USER /home/vagrant/apiserver-details/k3s.yaml
+sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 
 sudo sed -i -e "s/127.0.0.1/${MASTER_IP}/g" /home/vagrant/apiserver-details/k3s.yaml
 
@@ -20,6 +20,9 @@ sudo chown $USER:$USER /home/vagrant/apiserver-details/node-token
 #open 6443
 sudo firewall-cmd --permanent --add-port=6443/tcp
 sudo firewall-cmd --reload
+
+#helm
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 #cilium
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
