cd57679c05241f12506a1f3d0ac0aeacf4dd97db
ashupednekar <ashutoshpednekar15@gmail.com>
2025-01-31 17:39:19 +0530

    feat: added cilium to master node, got node to ready state

--- master.sh
+++ master.sh
@@ -5,11 +5,11 @@ sudo chown -R $USER:$USER /home/vagrant/apiserver-details
 export MASTER_IP=$(ip a | grep global | grep -v '10.0.2.15' | awk '{print $2}' | cut -f1 -d '/')
 echo $MASTER_IP > /home/vagrant/apiserver-details/master-ip
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-backend=none --disable=servicelb --disable=traefik --node-ip=${MASTER_IP} --node-external-ip=${MASTER_IP} --bind-address=${MASTER_IP}" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
 
-
-sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
+#apiserver details
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/apiserver-details/k3s.yaml
+sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 sudo chown $USER:$USER /home/vagrant/apiserver-details/k3s.yaml
 
 sudo sed -i -e "s/127.0.0.1/${MASTER_IP}/g" /home/vagrant/apiserver-details/k3s.yaml
@@ -17,4 +17,19 @@ sudo sed -i -e "s/127.0.0.1/${MASTER_IP}/g" /home/vagrant/apiserver-details/k3s.
 sudo cp /var/lib/rancher/k3s/server/node-token /home/vagrant/apiserver-details/node-token
 sudo chown $USER:$USER /home/vagrant/apiserver-details/node-token
 
+#open 6443
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --reload
 
+#cilium
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
+cilium install --version 1.16.6 --set=ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16"
+cilium status --wait
